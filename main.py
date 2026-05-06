c26db89f0bdbb5ecde8b7950ba9d5c378fba4440
Jonyboylovespie <jonathan.frawley@outlook.com>
2026-05-06 06:24:53 -0400

    asd

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from pathlib import Path
 IMAGES_DIR = Path(__file__).parent / "Images"
 appName = "com.ea.gp.pvzheroes"
 d = u2.connect()
+print(d.shell("wm size"))
+print(d.shell("wm density"))
 
 # I need to set a resolution for the image match, cause resizing window breaks ts
 

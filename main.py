2e754b2ad182cf356cce749fa475b27ba5b3f835
Jonyboylovespie <jonathan.frawley@outlook.com>
2026-05-06 06:28:29 -0400

    fix

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pathlib import Path
 IMAGES_DIR = Path(__file__).parent / "Images"
 appName = "com.ea.gp.pvzheroes"
 d = u2.connect()
-#1440x2560 and 360dpi
+#2560x1440 and 360dpi
 
 # I need to set a resolution for the image match, cause resizing window breaks ts
 
